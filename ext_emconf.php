1dcc8c209779f8d5e85dfde0bb020f988fe76f7c
Sebastian Michaelsen <sebastian.michaelsen@t3seo.de>
2013-01-14 22:03:30 +0100

    TER-Release: 0.4.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "oneclicklogin".
 #
-# Auto generated 27-04-2012 16:05
+# Auto generated 14-01-2013 22:01
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'For enabled users a 1-Click-Login link is shown at the Backend login screen to log in via OpenID. WARNING: The usernames and OpenID-Identifiers of the enabled users are shown at the login page and are visible for everyone.',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '0.4.0',
+	'version' => '0.4.1',
 	'dependencies' => 'openid',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:32:"class.tx_oneclicklogin_hooks.php";s:4:"57a0";s:21:"ext_conf_template.txt";s:4:"6e6e";s:12:"ext_icon.gif";s:4:"d477";s:17:"ext_localconf.php";s:4:"7ffa";s:14:"ext_tables.php";s:4:"1084";s:14:"ext_tables.sql";s:4:"25ae";s:16:"locallang_db.xml";s:4:"ed03";s:30:"Classes/Xclass/ux_SC_index.php";s:4:"ffc3";s:14:"doc/manual.pdf";s:4:"a58a";s:14:"doc/manual.sxw";s:4:"4790";}',
+	'_md5_values_when_last_written' => 'a:10:{s:32:"class.tx_oneclicklogin_hooks.php";s:4:"e003";s:21:"ext_conf_template.txt";s:4:"6e6e";s:12:"ext_icon.gif";s:4:"d477";s:17:"ext_localconf.php";s:4:"7ffa";s:14:"ext_tables.php";s:4:"1084";s:14:"ext_tables.sql";s:4:"25ae";s:16:"locallang_db.xml";s:4:"ed03";s:30:"Classes/Xclass/ux_SC_index.php";s:4:"ffc3";s:14:"doc/manual.pdf";s:4:"a58a";s:14:"doc/manual.sxw";s:4:"4790";}',
 	'suggests' => array(
 	),
 );
